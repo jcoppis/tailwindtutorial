c816e2c45c1d0e7af30f6ff8889628e8dbe4d979
Javier Coppis <jcoppis@grupoassa.com>
2020-06-26 14:22:05 -0300

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="bg-gray-200 min-h-screen p-8 frex items-center justify-center">
+      <PropertyCard :property="property" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PropertyCard from './components/PropertyCard'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    PropertyCard
+  },
+  data: () => ({
+    property: {
+      imageUrl: 'https://images.unsplash.com/photo-1580587771525-78b9dba3b914?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80',
+      imageAlt: 'lalala',
+      beds: 3,
+      baths: 2,
+      title: 'Modern home in city center',
+      priceInCents: 190000,
+      formattedPrice: '$1,900.00',
+      reviewCount: 34,
+      rating: 4,
+    }
+  })
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
